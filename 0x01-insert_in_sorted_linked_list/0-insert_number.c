12ce3ad6354684529d0e9bb4969c5ff1cc4139d0
peluza <edisonisaza@gmail.com>
2020-12-15 18:56:02 -0500

    0-insert_number.c

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * *insert_node - listint_t *insert_node
+ * @head: pointer to list to be freed
+ * @number: value to insert
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	while (current->next != NULL)
+		if (current->next->n < new->n)
+			current = current->next;
+		else
+			break;
+	new->next = current->next;
+	current->next = new;
+	return (new);
+}
