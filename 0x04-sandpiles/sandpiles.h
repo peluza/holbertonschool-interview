b2cac72634e91bc16f4cded760a507720cbb7f4b
peluza <edisonisaza@gmail.com>
2021-01-14 20:08:05 -0500

    sandpiles.h

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void p(int grid[3][3]);
+void sandpiles(int grid3[3][3], int grid1[3][3], int i , int j );
+
+
+#endif /* SANDPILES_H */
