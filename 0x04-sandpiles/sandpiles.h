67c2e818755d58f3e8140a08077765fa6b927fd8
peluza <edisonisaza@gmail.com>
2021-01-14 23:31:33 -0500

    new line

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -7,6 +7,7 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void print_grid_(int grid[3][3]);
 void sandpiles(int grid3[3][3], int grid1[3][3], int i, int j);
+void sandpiles_sum_(int grid1[3][3], int grid3[3][3], int grid4[3][3]);
 
 
 #endif /* SANDPILES_H */
