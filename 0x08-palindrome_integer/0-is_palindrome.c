0d4b71cbf9d8f7565e0a773f2a8cfc8d99a3fbbb
peluza <edisonisaza@gmail.com>
2021-02-11 10:59:57 -0500

    Palindrome unsigned integer

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - unsigned integer is a palindrome.
+ *
+ * @n: is the number to be checked
+ *
+ * Return: Return 1 if n is a palindrome, and 0 otherwise
+ */
+
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long numer_reverse = 0, tmp_1, tmp_2 = n;
+
+	if (n < 10)
+		return (0);
+
+	while (tmp_2 != 0)
+	{
+		tmp_1 = tmp_2 % 10;
+		numer_reverse = numer_reverse * 10 + tmp_1;
+		tmp_2 /= 10;
+	}
+	/**
+	* printf("%ld is n.  /n", n);
+	* printf("%ld is number_reverse. /n", numer_reverse);
+	*/
+	if (n == numer_reverse)
+		return  (1);
+	else
+		return (0);
+}
