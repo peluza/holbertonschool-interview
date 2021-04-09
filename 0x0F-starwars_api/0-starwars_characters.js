7c6f95b70e22a60bcd6260c3fe00060ff6ed4b3f
peluza <edisonisaza@gmail.com>
2021-04-08 19:51:04 -0500

    delete comment

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-// prints all characters of a Star Wars movie
 const request = require("request");
 const ID = process.argv.slice(2, 3);
 const URL = `https://swapi-api.hbtn.io/api/films/${ID}`;
