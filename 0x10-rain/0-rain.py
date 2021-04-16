be1b62fa858723a9a8aee4ff78e568a977afd797
peluza <edisonisaza@gmail.com>
2021-04-15 22:40:25 -0500

    Rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""rain"""
+
+def rain(walls):
+    """rain
+
+    Args:
+        walls (list): is a list of non-negative integers.
+
+    Returns:
+        [type]: Integer indicating total amount of rainwater retained.
+    """
+    if walls is None:
+        return 0
+
+    LenWalls = len(walls)
+    EndWalls = LenWalls -1
+    TotalWalter = 0
+
+    for i in range(1, EndWalls):
+        Left = walls[i]
+        for j in range(i):
+            Left = max(walls[j], Left)
+        Rigth = walls[i]
+        start = i + 1
+        for j in range(start, LenWalls):
+            Rigth = max(walls[j], Rigth)
+
+        MinWlater = min(Left, Rigth) -walls[i]
+        TotalWalter = TotalWalter + MinWlater
+
+    return TotalWalter
+
