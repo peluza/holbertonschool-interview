8a3f8fb3e453818927c19a94537d90729a3a5e23
Edison <edisonisaza@gmail.com>
2021-06-10 21:12:35 -0500

    header

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,8 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
