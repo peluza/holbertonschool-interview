b88ffbe454c392f5ec5d3c8b35afe2e0b1c37a35
Edison <edisonisaza@gmail.com>
2021-06-10 21:33:01 -0500

    pep8

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """rotate 2d matrix"""
 
+
 def rotate_2d_matrix(matrix):
     """rotate_2d_matrix
 
@@ -12,7 +13,7 @@ def rotate_2d_matrix(matrix):
     len_m = len(matrix)
     for col in range(len_m):
         tmp = []
-        for row in range(len_m-1, -1, -1):
+        for row in range(len_m - 1, -1, -1):
             tmp.append(matrix[row][col])
         new_matrix.append(tmp)
     for i in range(len_m):
